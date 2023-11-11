bfbdb725456f457b9b529c70d97e7d7dfc4c7770
trgordonb <gordoncheng@outlook.com>
2023-11-11 22:50:26 +0800

    deploy app

--- src/djambda/settings.py
+++ src/djambda/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    #"djambda",
     'core',
     'core.user',
     'rest_framework'
