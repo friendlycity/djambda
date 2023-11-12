ac8f258704f466828ad37486f4f17e15743535da
trgordonb <gordoncheng@outlook.com>
2023-11-12 14:27:12 +0800

    add default permission class

--- src/djambda/settings.py
+++ src/djambda/settings.py
@@ -48,7 +48,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication',),
     'DEFAULT_FILTER_BACKENDS':['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 15,
+    'DEFAULT_PERMISSION_CLASSES': ['permissions.IsAuthenticatedOrReadOnly'],
+    'PAGE_SIZE': 10,
 }
 
 MIDDLEWARE = [
