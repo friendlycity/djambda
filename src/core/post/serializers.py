0548bbc72162ab694f389be23e365f89e07d54c0
trgordonb <gordoncheng@outlook.com>
2023-11-12 12:04:06 +0800

    rewrite post serialized object

--- src/core/post/serializers.py
+++ src/core/post/serializers.py
@@ -1,12 +1,19 @@
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 from core.abstract.serializers import AbstractSerializer
+from core.user.serializers import UserSerializer
 from core.post.models import Post
 from core.user.models import User
 
 class PostSerializer(AbstractSerializer):
     author = serializers.SlugRelatedField(queryset=User.objects.all(), slug_field='public_id')
     
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        author = User.objects.get_object_by_public_id(rep["author"])
+        rep["author"] = UserSerializer(author).data
+        return rep
+
     def validate_author(self, value):
         if self.context["request"].user != value:
             raise ValidationError("You can't create a post for another user.")
