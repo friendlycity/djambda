bcec3f357b22422d633137615165099b6e486f90
trgordonb <gordoncheng@outlook.com>
2023-11-27 12:16:21 +0800

    use MySQLdb

--- src/core/management/commands/createdb.py
+++ src/core/management/commands/createdb.py
@@ -1,7 +1,6 @@
-from MySQLdb import _mysql, DatabaseError
+import MySQLdb
 from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
-
 
 class Command(BaseCommand):
     help = "Creates database command"
@@ -11,16 +10,16 @@ class Command(BaseCommand):
         parser.add_argument("--exist_ok", action="store_true")
 
     def handle(self, *args, **options):
-        connection = _mysql.connect(
+        db = MySQLdb.connect(
             user=settings.DATABASES["default"]["USER"],
             passwd=settings.DATABASES["default"]["PASSWORD"],
             host=settings.DATABASES["default"]["HOST"],
             port=settings.DATABASES["default"]["PORT"]
         )
         try:
-            with connection.cursor() as cursor:
+            with db.cursor() as cursor:
                 cursor.execute(f"CREATE DATABASE \"{options['db_name']}\"")
-        except DatabaseError:
+        except MySQLdb.DatabaseError:
             if not options["exist_ok"]:
                 raise CommandError('Database "%s" already exists' % options["db_name"])
         else:
